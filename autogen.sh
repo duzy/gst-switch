287562a83f4a2ce8c05e1dc87aa4d136a800de6c
Duzy Chan <code@duzy.info>
2012-12-17 17:51:32 +0800

    initial commit, with the following:
      *) tcpmixsrc
      *) gst-switch-srv (not finished)
      *) gst-switch-ui (not finished)

---
+++ autogen.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# you can either set the environment variables AUTOCONF, AUTOHEADER, AUTOMAKE,
+# ACLOCAL, AUTOPOINT and/or LIBTOOLIZE to the right versions, or leave them
+# unset and get the defaults
+
+if [ -f "autoregen.sh" ]; then
+  rm autoregen.sh
+fi
+echo "#!/bin/sh" > autoregen.sh
+echo "./autogen.sh $@ \$@" >> autoregen.sh
+chmod +x autoregen.sh
+
+autoreconf --verbose --force --install --make || {
+ echo 'autogen.sh failed';
+ exit 1;
+}
+
+./configure $@ || {
+ echo 'configure failed';
+ exit 1;
+}
+
+echo
+echo "Now type 'make' to compile this module."
+echo
