193d5e18a675bf09c643c1bb6d3a514381106b4e
Duzy Chan <code@duzy.info>
2013-02-01 11:58:22 +0800

    refactoring test utility

---
+++ tests/names.mk
@@ -0,0 +1,14 @@
+TESTS = \
+	test-video \
+	test-audio \
+	test-fuzz \
+	test-controller \
+	test-composite-mode \
+	$(null)
+
+UI_TESTS = \
+	test-ui \
+	test-switching \
+	test-random \
+	test-checking-timestamps \
+	$(null)
