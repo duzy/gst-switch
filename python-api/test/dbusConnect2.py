6d446c7eeb90061301a202a44d05b624b88e54de
hyades <aayushahuja@gmail.com>
2013-06-18 22:20:53 +0530

    testing extending python using c

--- python-api/test/dbusConnect2.py
+++ python-api/test/dbusConnect2.py
@@ -8,17 +8,13 @@ connection = Gio.DBusConnection.new_for_address_sync(
                     address,
                     Gio.DBusConnectionFlags.AUTHENTICATION_CLIENT,
                     None, None)
-a =  connection.call_sync(
-            name, object_path, 'org.freedesktop.DBus.Introspectable', 'Introspect',
-            None, GLib.VariantType.new("(s)"), Gio.DBusCallFlags.NONE, -1,
-            None)
-mode = 0
+
+mode = 1
 args = GLib.Variant('(i)',(mode,))
-print "args:::::::", args
+print "args:", args
 b =  connection.call_sync(
             name, object_path, 'info.duzy.gst_switch.SwitchControllerInterface', 'set_composite_mode',
             args, GLib.VariantType.new("(b)"), Gio.DBusCallFlags.NONE, -1,
             None)
 
 print b
-print type(a), type(b)
---
+++ python-api/test/spam.c
@@ -0,0 +1,39 @@
+#include <Python.h>
+
+static PyObject *
+spam_system(PyObject *self, PyObject *args)
+{
+    const char *command;
+    int sts;
+
+    if (!PyArg_ParseTuple(args, "s", &command))
+        return NULL;
+    sts = system(command);
+    return Py_BuildValue("i", sts);
+}
+
+static PyMethodDef SpamMethods[] = {
+    {"system",  spam_system, METH_VARARGS,
+     "Execute a shell command."},
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+PyMODINIT_FUNC
+initspam(void)
+{
+    (void) Py_InitModule("spam", SpamMethods);
+}
+
+int
+main(int argc, char *argv[])
+{
+    /* Pass argv[0] to the Python interpreter */
+    Py_SetProgramName(argv[0]);
+
+    /* Initialize the Python interpreter.  Required. */
+    Py_Initialize();
+
+    /* Add a static module */
+    initspam();
+}
+
