c3868dc2da9cf47f0c62b994642c7bad1860f46c
hyades <aayushahuja@gmail.com>
2013-06-19 17:30:22 +0530

    added test for changing pip modes

--- python-api/gstswitch/test.py
+++ python-api/gstswitch/test.py
@@ -1,12 +1,16 @@
 from gstswitch import *
 from time import sleep
 import subprocess
+
 s = Server()
 sleep(0.5)
-u = UI()
+#u = UI()
+cmd = "gst-launch-1.0 tcpclientsrc port=3001 ! gdpdepay ! autovideosink"
+proc = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, bufsize=-1, shell=False)
 s.new_test_video()
-sleep(5)
 s.new_test_video(clockoverlay=True)
-sleep(15)
-s.end()
+#wait for user to end the server
+raw_input()
 proc.kill()
+s.end()
+
--- python-api/test/dbusConnect2.py
+++ python-api/test/dbusConnect2.py
@@ -3,21 +3,30 @@ from time import sleep
 
 address = "unix:abstract=gstswitch"
 name = None
-object_path = "/info/duzy/gst_switch/SwitchController"
-connection = Gio.DBusConnection.new_for_address_sync(
+object_path = "/info/duzy/gst/switch/SwitchController"
+print "mode:"
+
+
+# a = connection.call_sync(
+#             name, object_path, 'org.freedesktop.DBus.Introspectable', 'Introspect',
+#             None, GLib.VariantType.new("(s)"), Gio.DBusCallFlags.NONE, -1,
+#             None)
+#mode = 1
+#print type(mode), mode
+mode=0
+while mode<=3:
+    connection = Gio.DBusConnection.new_for_address_sync(
                     address,
                     Gio.DBusConnectionFlags.AUTHENTICATION_CLIENT,
                     None, None)
-b =  connection.call_sync(
-            name, object_path, 'info.duzy.gst_switch.SwitchControllerInterface', 'set_composite_mode',
-            args, GLib.VariantType.new("(b)"), Gio.DBusCallFlags.NONE, -1,
-            None)
-mode = 1
-args = GLib.Variant('(i)',(mode,))
-print "args:", args
-b =  connection.call_sync(
-            name, object_path, 'info.duzy.gst_switch.SwitchControllerInterface', 'set_composite_mode',
-            args, GLib.VariantType.new("(b)"), Gio.DBusCallFlags.NONE, -1,
-            None)
-
-print b
+    args = GLib.Variant('(i)',(mode,))
+    print "args:", args
+    b =  connection.call_sync(
+                name, object_path, 'info.duzy.gst.switch.SwitchControllerInterface', 'set_composite_mode',
+                args, GLib.VariantType.new("(b)"), Gio.DBusCallFlags.NONE, -1,
+                None)
+    print b
+    mode += 1
+    if mode==4:
+        mode=0
+    sleep(5)
