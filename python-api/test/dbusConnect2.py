7df8ad7ea2935391c870e96dc64877b61e5afc99
hyades <aayushahuja@gmail.com>
2013-06-19 10:58:25 +0530

    updated the introspect.txt

--- python-api/test/dbusConnect2.py
+++ python-api/test/dbusConnect2.py
@@ -8,7 +8,10 @@ connection = Gio.DBusConnection.new_for_address_sync(
                     address,
                     Gio.DBusConnectionFlags.AUTHENTICATION_CLIENT,
                     None, None)
-
+b =  connection.call_sync(
+            name, object_path, 'info.duzy.gst_switch.SwitchControllerInterface', 'set_composite_mode',
+            args, GLib.VariantType.new("(b)"), Gio.DBusCallFlags.NONE, -1,
+            None)
 mode = 1
 args = GLib.Variant('(i)',(mode,))
 print "args:", args
