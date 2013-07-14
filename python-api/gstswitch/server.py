f78f8d1aa35e58996a01fceb2c26d10fc8d76c42
hyades <aayushahuja@gmail.com>
2013-07-15 01:18:11 +0530

    Documentation server.py

--- python-api/gstswitch/server.py
+++ python-api/gstswitch/server.py
@@ -4,44 +4,86 @@ import signal
 import subprocess
 
 from exception import *
-from controller import Controller
 from time import sleep
 
 
 class BaseServer(object):
-    """Properties of the Server
+    """Class controlling Basic Properties of the Server
+
+    :param: None
     """
     def __init__(self):
         super(BaseServer, self).__init__()
 
     def set_video_port(self, video_port):
+        """Sets the server's video port
+
+        :param video_port: Video port
+        :returns: nothing
+        """
         self.VIDEO_PORT = str(video_port)
 
     def set_audio_port(self, audio_port):
+        """Sets the server's audio port
+
+        :param audio_port: Audio port
+        :returns: nothing
+        """
         self.AUDIO_PORT = str(audio_port)
 
     def set_control_port(self, control_port):
+        """Sets the server's control port
+
+        :param control_port: Control port
+        :returns: nothing
+        """
         self.CONTROL_PORT = str(control_port)
 
     def set_record_file(self, record_file):
+        """Sets the record file name format - [name-date-time.datas]
+
+        :param record_file: The record file name
+        :returns: nothing
+        """
         self.RECORD_FILE = record_file
 
     def get_video_port(self):
+        """Returns the video port number
+
+        :param: None
+        :returns: Video port
+        """
         return self.VIDEO_PORT
 
     def get_audio_port(self):
+        """Returns the audio port number
+
+        :param: None
+        :returns: Audio port
+        """
         return self.AUDIO_PORT
 
     def get_control_port(self):
+        """Returns the control port number
+
+        :param: None
+        :returns: Control port
+        """
         return self.CONTROL_PORT
 
     def get_record_file(self):
+        """Returns the record file format
+
+        :param: None
+        :returns: Record file format
+        """
         return self.RECORD_FILE
 
 
 class ServerProcess(object):
-    """Handles all processes created. This includes the server process
-    and test sources added to the gst-switch-srv
+    """Class handling controlling the server process
+
+    :param: None
     """
 
     def __init__(self):
@@ -49,19 +91,25 @@ class ServerProcess(object):
         pass
 
     def run(self):
-        """Launches the server
+        """Launches the server process
+
+        :param: None
+        :returns: nothing
         """
         self.proc = None
         self.pid = -1
         print "Starting server"
-        self.proc = self.run_process()
+        self.proc = self._run_process()
         if self.proc is None:
             pass
         else:
             self.pid = self.proc.pid
+        # TODO: Sleep time may vary
         sleep(0.5)
 
-    def run_process(self):
+    def _run_process(self):
+        """Private method for running gst-switch-srv process
+        """
         cmd = self.PATH
         # cmd = ''
         cmd += """gst-switch-srv \
@@ -69,7 +117,7 @@ class ServerProcess(object):
                     --audio-input-port=%s \
                     --control-port=%s \
                     --record=%s """ % (self.VIDEO_PORT, self.AUDIO_PORT, self.CONTROL_PORT, self.RECORD_FILE)
-        proc = self.start_process(cmd)
+        proc = self._start_process(cmd)
         print "process:", proc
         if proc is None:
             print 'ERROR: Server unable to create process'
@@ -78,19 +126,22 @@ class ServerProcess(object):
             print 'Created process with PID:%s', str(proc.pid)
             return proc
 
-    def start_process(self, cmd):
+    def _start_process(self, cmd):
+        """Private method for starting a process
+
+        :param cmd: The command which needs to be excecuted
+        :returns: process created
+        """
         print 'Creating process %s' % (cmd)
         with open(os.devnull, 'w') as tempf:
             process = subprocess.Popen(cmd.split(), stdout=tempf, stderr=tempf,  bufsize=-1, shell=False)
         return process
 
     def terminate(self):
-        """Stops the server
-        Returns:
-            True on success
-            False on failure
-        Parameters:
-            None
+        """Terminates the server
+
+        :param: None
+        :returns: True when success
         """
         print 'Killing server'
         proc = self.proc
@@ -104,27 +155,35 @@ class ServerProcess(object):
         return ret
 
     def kill(self):
+        """Kills the server process by sending SIGKILL
+
+        :param: None
+        :returns: nothing
+        """
         os.kill(self.pid, signal.SIGKILL)
 
     def set_executable_path(self, path):
+        """Sets the path where all exceutables
+        gst-switch-srv, gst-launch-1.0, wtc are located
+
+        :param path: Path where exceutables are present
+        :returns: nothing
+        """
         self.PATH = path
 
 
 class Server(BaseServer, ServerProcess):
-    """Controls all Server operations and Test Video Sources
+    """Controls all Server operations
+
+    :param path: Path where all exceutables gst-switch-srv, gst-launch-1.0, wtc are located
+    :param video_port: The video port number - default = 3000
+    :param audio_port: The audio port number - default = 4000
+    :param control_port: The control port number - default = 5000
+    :param record_file: The record file format
+    :returns: nothing
     """
 
     def __init__(self, path, video_port=3000, audio_port=4000, control_port=5000, record_file='record.data'):
-        """Contructor for the Server class
-        Returns:
-            Server() object
-        Parameters:
-            video_port(optional)
-            audio_port(optional)
-            control_port(optional)
-            record_file(optional)
-
-        """
         super(Server, self).__init__()
         self.set_executable_path(path)
         self.set_video_port(video_port)
