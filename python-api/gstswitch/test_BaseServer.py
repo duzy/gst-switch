ab2743e221b45c980a75a9e9464f6ea61f6748a0
hyades <aayushahuja@gmail.com>
2013-07-16 21:43:30 +0530

    Added unittest for BaseServer.set_control_port

--- python-api/gstswitch/test_BaseServer.py
+++ python-api/gstswitch/test_BaseServer.py
@@ -90,3 +90,48 @@ class TestSetAudioPort(object):
         instance = BaseServer()
         instance.set_audio_port(port)
         assert port == int(instance.AUDIO_PORT)
+
+
+class TestSetControlPort(object):
+
+    def test_negative(self):
+        port = -3
+        instance = BaseServer()
+        with pytest.raises(ValueError):
+            instance.set_control_port(port)
+
+    def test_zero(self):
+        port = 0
+        instance = BaseServer()
+        with pytest.raises(ValueError):
+            instance.set_control_port(port)
+
+    def test_huge(self):
+        port = 1e6
+        instance = BaseServer()
+        with pytest.raises(ValueError):
+            instance.set_control_port(port)
+
+    def test_none(self):
+        port = None
+        instance = BaseServer()
+        with pytest.raises(TypeError):
+            instance.set_control_port(port)
+
+    def test_string(self):
+        port = ""
+        instance = BaseServer()
+        with pytest.raises(TypeError):
+            instance.set_control_port(port)
+
+    def test_dict(self):
+        port = {}
+        instance = BaseServer()
+        with pytest.raises(TypeError):
+            instance.set_control_port(port)
+
+    def test_normal(self):
+        port = 3000
+        instance = BaseServer()
+        instance.set_control_port(port)
+        assert port == int(instance.CONTROL_PORT)
