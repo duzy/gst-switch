80da75070f0c0c0d3c7a3f32e3a6350804a60984
hyades <aayushahuja@gmail.com>
2013-08-17 03:14:12 +0530

    integration tests - get_encode_port

--- python-api/tests/integrationtests/test_controller.py
+++ python-api/tests/integrationtests/test_controller.py
@@ -37,11 +37,12 @@ class TestEstablishConnection(object):
 class TestGetComposePort(object):
 
     NUM = 4
+    FACTOR = 100
     def get_compose_port(self):
         r = []
         controller = Controller()
         controller.establish_connection()
-        for i in range(self.NUM*3):
+        for i in range(self.NUM*self.FACTOR):
             r.append(controller.get_compose_port())
         return r
 
@@ -50,7 +51,7 @@ class TestGetComposePort(object):
         expected_result = []
         for i in range(self.NUM):
             video_port = (i+1)*1000
-            expected_result.append([video_port+1]*self.NUM*3)
+            expected_result.append([video_port+1]*self.NUM*self.FACTOR)
             s = Server(path=PATH, video_port=video_port)
             try:
                 s.run()
@@ -67,6 +68,45 @@ class TestGetComposePort(object):
         at = [ tuple(i) for i in expected_result]
         bt = [ tuple(i) for i in res]
         assert set(at) == set(bt)
+
+
+class TestGetEncodePort(object):
+
+    NUM = 4
+    FACTOR = 100
+    def get_encode_port(self):
+        r = []
+        controller = Controller()
+        controller.establish_connection()
+        for i in range(self.NUM*self.FACTOR):
+            r.append(controller.get_encode_port())
+        return r
+
+    def test_encode_ports(self):
+        res = []
+        expected_result = []
+        for i in range(self.NUM):
+            video_port = (i+1)*1000
+            expected_result.append([video_port+2]*self.NUM*self.FACTOR)
+            s = Server(path=PATH, video_port=video_port)
+            try:
+                s.run()
+                sources = TestSources(video_port=video_port)
+                sources.new_test_video()
+                sources.new_test_video()
+
+                res.append(self.get_encode_port())
+                s.terminate()
+            finally:
+                if s.proc:
+                    s.terminate()
+        
+        at = [ tuple(i) for i in expected_result]
+        bt = [ tuple(i) for i in res]
+        assert set(at) == set(bt)
+
+
+
 
 
 
