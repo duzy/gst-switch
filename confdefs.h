e247c4d3e75ece4e1a0dff6755837230d6540863
hyades <aayushahuja@gmail.com>
2013-08-29 22:39:56 +0530

    removed libtoolize dependency

---
+++ confdefs.h
@@ -0,0 +1,21 @@
+/* confdefs.h */
+#define PACKAGE_NAME "gst-switch"
+#define PACKAGE_TARNAME "gst-switch"
+#define PACKAGE_VERSION "0.0.1"
+#define PACKAGE_STRING "gst-switch 0.0.1"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_URL ""
+#define PACKAGE "gst-switch"
+#define VERSION "0.0.1"
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_DLFCN_H 1
+#define LT_OBJDIR ".libs/"
