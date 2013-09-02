9fa90a562360fcd8e3b42280af7df77a089670f4
hyades <aayushahuja@gmail.com>
2013-09-03 02:44:53 +0530

    modified conf.py for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,9 @@
 # serve to show the default.
 
 import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(__file__, "../../python-api")))
-sys.path.insert(0, os.path.abspath(os.path.join(__file__, "../fake-lib")))
-# print sys.path
+sys.path.insert(0, os.path.abspath('../python-api'))
+sys.path.insert(0, os.path.abspath('fake-dir'))
+print sys.path
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
