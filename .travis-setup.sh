fe64342676d4ec5bbd779dd6ffca0580ad9d8bee
hyades <aayushahuja@gmail.com>
2013-09-06 20:18:30 +0530

    modified travis file

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -8,19 +8,19 @@ sudo apt-get install python-gi python3-gi gstreamer1.0-tools gir1.2-gstreamer-1.
 sudo apt-get install python-scipy ffmpeg
 sudo apt-get build-dep gstreamer1.0
 sudo apt-get install autoconf automake autopoint libbz2-dev libdv4-dev libfaac-dev libfaad-dev libgtk-3-dev libmjpegtools-dev libtag1-dev libasound2-dev libtool libvpx-dev libxv-dev libx11-dev libogg-dev libvorbis-dev libopencv-dev libcv-dev libhighgui-dev libv4l-dev pkg-config zlib1g-dev gtk-doc-tools yasm bison flex
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/
+export LD_LIBRARY_PATH=/usr/local/lib/
 git clone http://git.chromium.org/webm/libvpx.git
 cd libvpx
 git checkout v1.2.0
-./configure --enable-shared --enable-vp8
+./configure --enable-shared --enable-vp8 --prefix=/usr/local
 make
 sudo make install
 cd ..
-export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
-export LD_LIBRARY_PATH=/usr/local/lib
 git clone git://anongit.freedesktop.org/gstreamer/gstreamer
 cd gstreamer
 git checkout 6c11da1
-./autogen.sh
+./autogen.sh --prefix=/usr/local
 make clean
 make
 sudo make install
@@ -28,7 +28,7 @@ cd ..
 git clone git://anongit.freedesktop.org/gstreamer/gst-plugins-base
 cd gst-plugins-base
 git checkout a8df760
-./autogen.sh
+./autogen.sh --prefix=/usr/local
 make clean
 make
 sudo make install
@@ -36,7 +36,7 @@ cd ..
 git clone git://anongit.freedesktop.org/gstreamer/gst-plugins-good
 cd gst-plugins-good
 git checkout d14d4c4
-./autogen.sh --enable-experimental
+./autogen.sh --enable-experimental --prefix=/usr/local
 make clean
 make
 sudo make install
@@ -44,7 +44,7 @@ cd ..
 git clone git://anongit.freedesktop.org/gstreamer/gst-plugins-ugly
 cd gst-plugins-ugly
 git checkout 68985ba
-./autogen.sh
+./autogen.sh --prefix=/usr/local
 make clean
 make
 sudo make install
@@ -53,12 +53,12 @@ git clone https://github.com/duzy/gst-plugins-bad.git
 cd gst-plugins-bad
 git checkout speakertrack_new
 git checkout 
-./autogen.sh
+./autogen.sh --prefix=/usr/local
 make clean
 make
 sudo make install
 cd ..
-./autogen.sh
+./autogen.sh --prefix=/usr/local
 make clean
 make
 sudo make install
