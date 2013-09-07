e669fa3b313c3f80f9cfe50d2a4188691af96b17
hyades <aayushahuja@gmail.com>
2013-09-08 02:43:40 +0530

    lint and docs test_controller.py

--- python-api/tests/integrationtests/generate_reference_frames.py
+++ python-api/tests/integrationtests/generate_reference_frames.py
@@ -1,15 +1,20 @@
+"""
+Used for generating reference frames
+Usually should be run just once
+"""
+
 from compare import GenerateReferenceFrames
 from test_controller import TestSetCompositeMode, TestAdjustPIP
 
 def set_composite_mode_ref_frames(mode):
-
+    """Set the composite mode for indexing the frames"""
     test = 'composite_mode_{0}'.format(mode)
     video = 'output-{0}.data'.format(mode)
     gen = GenerateReferenceFrames(test, video)
     gen.generate_frames()
 
 def adjust_pip_ref_frames(index):
-
+    """Set the index of frames"""
     test = 'adjust_pip_{0}'.format(index)
     video = 'output-{0}.data'.format(index)
     gen = GenerateReferenceFrames(test, video)
@@ -17,7 +22,7 @@ def adjust_pip_ref_frames(index):
 
 
 def main():
-
+    """Generate reference frames"""
     test1 = TestSetCompositeMode()
     for i in range(4):
         test1.set_composite_mode(i, True)
@@ -25,14 +30,18 @@ def main():
 
 
     test2 = TestAdjustPIP()
-    d = [
+    dic = [
                 [50, 75, 0, 0],
         ]
     for i in range(4, 5):
-        test2.adjust_pip(d[i-4][0], d[i-4][1], d[i-4][2], d[i-4][3], i, True)
+        test2.adjust_pip(
+            dic[i-4][0],
+            dic[i-4][1],
+            dic[i-4][2],
+            dic[i-4][3],
+            i,
+            True)
         adjust_pip_ref_frames(i)
-
-
 
 
 if __name__ == '__main__':
