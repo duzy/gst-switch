c1dcec794d3265993f16977b0e7f1a38690d4a56
hyades <aayushahuja@gmail.com>
2013-09-08 12:04:13 +0530

    lint and docs for test_server.py

--- python-api/tests/integrationtests/test_server.py
+++ python-api/tests/integrationtests/test_server.py
@@ -1,39 +1,43 @@
+"""
+Integration tests for Server in server.py
+"""
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(__file__, "../../../")))
 
 from gstswitch.server import Server
-from gstswitch.helpers import *
-
 
 # PATH = os.getenv("HOME") + '/gst/stage/bin/'
 PATH = '/usr/local/bin/'
+
 
 class TestServerStartStop(object):
     """Test Starting and Stopping the Server
     Run Server and Stop multiple times
     """
     NUM = 5
-    
+
     def startstop(self):
-        s = Server(path=PATH)
+        """Start and Stop the Server"""
+        serv = Server(path=PATH)
         try:
-            s.run()
-            pid = s.pid
+            serv.run()
+            pid = serv.pid
             assert pid > 0
-            s.terminate(1)
-            assert s.proc is None
+            serv.terminate(1)
+            assert serv.proc is None
         finally:
-                if s.proc:
-                    poll = s.proc.poll()
-                    print self.__class__
-                    if poll == -11:
-                        print "SEGMENTATION FAULT OCCURRED"
-                    print "ERROR CODE - {0}".format(abs(poll))
-                    s.terminate(1)
-                    f = open('server.log')
-                    print f.read()
+            if serv.proc:
+                poll = serv.proc.poll()
+                print self.__class__
+                if poll == -11:
+                    print "SEGMENTATION FAULT OCCURRED"
+                print "ERROR CODE - {0}".format(abs(poll))
+                serv.terminate(1)
+                log = open('server.log')
+                print log.read()
 
     def test_start_stop(self):
-        for i in range(self.NUM):
+        """Test Start and Stop the Server"""
+        for _ in range(self.NUM):
             self.startstop()
