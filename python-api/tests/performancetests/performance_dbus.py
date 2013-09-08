d48a3f6e70ca26d937db284cdb2313e4b0ec951a
hyades <aayushahuja@gmail.com>
2013-09-09 04:35:05 +0530

    Performance Tests

--- python-api/tests/performancetests/performance_dbus.py
+++ python-api/tests/performancetests/performance_dbus.py
@@ -17,7 +17,7 @@ PATH = '/usr/local/bin/'
 
 
 def get_compose_port(num):
-        """Test get_compose_port"""
+        """Test get_compose_port - num times"""
         video_port = 8000
         serv = Server(path=PATH, video_port=video_port)
         try:
@@ -45,53 +45,40 @@ def get_compose_port(num):
                     print log.read()
 
 
-class TestGetComposePort100(object):
+class TestGetComposePort(object):
     """Performance test for get_compose_port method - 100 times"""
 
-    NUMBER_TEST_RUNS = 100
-
-    def test(self):
-        get_compose_port(self.NUMBER_TEST_RUNS)
-
-
-class TestGetComposePort200(object):
-    """Performance test for get_compose_port method - 200 times"""
-
-    NUMBER_TEST_RUNS = 200
-
-    def test(self):
-        get_compose_port(self.NUMBER_TEST_RUNS)
+    def test_100(self):
+        """Performance test for get_compose_port method"""
+        number_test_runs = 100
+        get_compose_port(number_test_runs)
 
 
-class TestGetComposePort300(object):
-    """Performance test for get_compose_port method - 300 times"""
+    def test_200(self):
+        """Performance test for get_compose_port method"""
+        number_test_runs = 200
+        get_compose_port(number_test_runs)
 
-    NUMBER_TEST_RUNS = 300
 
-    def test(self):
-        get_compose_port(self.NUMBER_TEST_RUNS)
+    def test_300(self):
+        """Performance test for get_compose_port method"""
+        number_test_runs = 300
+        get_compose_port(number_test_runs)
     
 
-class TestGetComposePort400(object):
-    """Performance test for get_compose_port method - 400 times"""
+    def test_400(self):
+        """Performance test for get_compose_port method"""
+        number_test_runs = 400
+        get_compose_port(number_test_runs)
 
-    NUMBER_TEST_RUNS = 400
-
-    def test(self):
-        get_compose_port(self.NUMBER_TEST_RUNS)
-
-
-class TestGetComposePort500(object):
-    """Performance test for get_compose_port method - 500 times"""
-
-    NUMBER_TEST_RUNS = 500
-
-    def test(self):
-        get_compose_port(self.NUMBER_TEST_RUNS)
+    def test_500(self):
+        """Performance test for get_compose_port method"""
+        number_test_runs = 500
+        get_compose_port(number_test_runs)
 
 
 def get_encode_port(num):
-        """Test get_encode_port"""
+        """Test get_encode_port - num times"""
         video_port = 3000
         serv = Server(path=PATH, video_port=video_port)
         try:
@@ -119,49 +106,33 @@ def get_encode_port(num):
                     print log.read()
 
 
-class TestGetEncodePort100(object):
-    """Performance test for get_encode_port method - 100 times"""
+class TestGetEncodePort(object):
+    """Performance test for get_encode_port method"""
 
-    NUMBER_TEST_RUNS = 100
+    def test_100(self):
+        """Performance test for get_encode_port method - 100 times"""
+        number_test_runs = 100
+        get_encode_port(number_test_runs)
 
-    def test(self):
-        get_encode_port(self.NUMBER_TEST_RUNS)
+    def test_200(self):
+        """Performance test for get_encode_port method - 200 times"""
+        number_test_runs = 200
+        get_encode_port(number_test_runs)
 
+    def test_300(self):
+        """Performance test for get_encode_port method - 300 times"""
+        number_test_runs = 300
+        get_encode_port(number_test_runs)
 
-class TestGetEncodePort200(object):
-    """Performance test for get_encode_port method - 200 times"""
+    def test_400(self):
+        """Performance test for get_encode_port method - 400 times"""
+        number_test_runs = 400
+        get_encode_port(number_test_runs)
 
-    NUMBER_TEST_RUNS = 200
-
-    def test(self):
-        get_encode_port(self.NUMBER_TEST_RUNS)
-
-
-class TestGetEncodePort300(object):
-    """Performance test for get_encode_port method - 300 times"""
-
-    NUMBER_TEST_RUNS = 300
-
-    def test(self):
-        get_encode_port(self.NUMBER_TEST_RUNS)
-    
-
-class TestGetEncodePort400(object):
-    """Performance test for get_encode_port method - 400 times"""
-
-    NUMBER_TEST_RUNS = 400
-
-    def test(self):
-        get_encode_port(self.NUMBER_TEST_RUNS)
-
-
-class TestGetEncodePort500(object):
-    """Performance test for get_encode_port method - 500 times"""
-
-    NUMBER_TEST_RUNS = 500
-
-    def test(self):
-        get_encode_port(self.NUMBER_TEST_RUNS)
+    def test_500(self):
+        """Performance test for get_encode_port method - 500 times"""
+        number_test_runs = 500
+        get_encode_port(number_test_runs)
 
 
 
@@ -195,47 +166,70 @@ def get_audio_port(num):
                     print log.read()
 
 
-class TestGetAudioPort100(object):
-    """Performance test for get_audio_port method - 100 times"""
+class TestGetAudioPort(object):
+ 
+    def test_100(self):
+        """Performance test for get_audio_port method - 100 times"""
+        number_test_runs = 100
+        get_audio_port(number_test_runs)
 
-    NUMBER_TEST_RUNS = 100
-
-    def test(self):
-        get_audio_port(self.NUMBER_TEST_RUNS)
-
-
-class TestGetAudioPort200(object):
-    """Performance test for get_audio_port method - 200 times"""
-
-    NUMBER_TEST_RUNS = 200
-
-    def test(self):
-        get_audio_port(self.NUMBER_TEST_RUNS)
-
-
-class TestGetAudioPort300(object):
-    """Performance test for get_audio_port method - 300 times"""
-
-    NUMBER_TEST_RUNS = 300
-
-    def test(self):
-        get_audio_port(self.NUMBER_TEST_RUNS)
     
+    def test_200(self):
+        """Performance test for get_audio_port method - 200 times"""
+        number_test_runs = 200
+        get_audio_port(number_test_runs)
 
-class TestGetAudioPort400(object):
-    """Performance test for get_audio_port method - 400 times"""
+    
+    def test_300(self):
+        """Performance test for get_audio_port method - 300 times"""
+        number_test_runs = 300
+        get_audio_port(number_test_runs)
+    
+    
+    def test_400(self):
+        """Performance test for get_audio_port method - 400 times"""
+        number_test_runs = 400
+        get_audio_port(number_test_runs)
 
-    NUMBER_TEST_RUNS = 400
+    
+    def test_500(self):
+        """Performance test for get_audio_port method - 500 times"""
+        number_test_runs = 500
+        get_audio_port(number_test_runs)
 
-    def test(self):
-        get_audio_port(self.NUMBER_TEST_RUNS)
+
+def get_preview_ports(num):
+    """Get Preview Ports when num number of sources are added"""
+    video_port = 3000
+    serv = Server(path=PATH, video_port=video_port)
+    try:
+        serv.run()
+        sources = TestSources(video_port=video_port)
+        sources.new_test_video()
+        
+        expected_res = [ i for i in range(3003, 3003 + num)]
+
+        controller = Controller()
+        controller.establish_connection()
+        res = []
+        for _ in range(num):
+            res.append(controller.get_encode_port())
+
+        assert expected_res == res
+    finally:
+        if serv.proc:
+                poll = serv.proc.poll()
+                if poll == -11:
+                    print "SEGMENTATION FAULT OCCURRED"
+                print "ERROR CODE - {0}".format(poll)
+                serv.terminate(1)
+                log = open('server.log')
+                print log.read()
 
 
-class TestGetAudioPort500(object):
-    """Performance test for get_audio_port method - 500 times"""
+class TestGetPreviewPorts(object):
 
-    NUMBER_TEST_RUNS = 500
-
-    def test(self):
-        get_audio_port(self.NUMBER_TEST_RUNS)
-
+    def test_100():
+        """Test when 100 sources are added"""
+        number_souces = 100
+        get_preview_ports(number_souces)
