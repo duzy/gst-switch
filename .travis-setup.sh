018dc5b2472ce682c6385fa0907ddc02f99430b4
hyades <aayushahuja@gmail.com>
2013-09-09 23:14:52 +0530

    Modified travis-setup.sh

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
+sudo apt-get -y install python-software-properties python-pip
 sudo pip install -r requirements.txt --use-mirrors
 echo 'yes' | sudo add-apt-repository ppa:gstreamer-developers/ppa
 sudo apt-get update
-sudo apt-get install libglib2.0-dev gir1.2-glib-2.0 libgirepository1.0-dev libglib2.0-0 
-sudo apt-get install python-gi python3-gi gstreamer1.0-tools gir1.2-gstreamer-1.0 gir1.2-gst-plugins-base-1.0 gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad gstreamer1.0-libav
-sudo apt-get install python-scipy ffmpeg
-sudo apt-get build-dep gstreamer1.0
-sudo apt-get install .*gstreamer.*1\.0.* libgtk-3-dev
-sudo apt-get install autoconf automake autopoint libbz2-dev libdv4-dev libfaac-dev libfaad-dev libgtk-3-dev libmjpegtools-dev libtag1-dev libasound2-dev libtool libvpx-dev libxv-dev libx11-dev libogg-dev libvorbis-dev libopencv-dev libcv-dev libhighgui-dev libv4l-dev pkg-config zlib1g-dev gtk-doc-tools yasm bison flex
+sudo apt-get -y install libglib2.0-dev gir1.2-glib-2.0 libgirepository1.0-dev libglib2.0-0 
+sudo apt-get -y install python-gi python3-gi gstreamer1.0-tools gir1.2-gstreamer-1.0 gir1.2-gst-plugins-base-1.0 gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad gstreamer1.0-libav
+sudo apt-get -y install python-scipy ffmpeg
+sudo apt-get -y build-dep gstreamer1.0
+sudo apt-get -y install autoconf automake autopoint libbz2-dev libdv4-dev libfaac-dev libfaad-dev libgtk-3-dev libmjpegtools-dev libtag1-dev libasound2-dev libtool libvpx-dev libxv-dev libx11-dev libogg-dev libvorbis-dev libopencv-dev libcv-dev libhighgui-dev libv4l-dev pkg-config zlib1g-dev gtk-doc-tools yasm bison flex
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/
 export LD_LIBRARY_PATH=/usr/local/lib/
 git clone http://git.chromium.org/webm/libvpx.git
