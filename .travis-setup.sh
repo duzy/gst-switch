3344f4efba345411e19cd1fb55a98fab7f2b5819
hyades <aayushahuja@gmail.com>
2013-09-10 23:06:13 +0530

    trying adding libfaac0 and libtheora-dev as dependencies

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -8,7 +8,7 @@ sudo apt-get -y install libglib2.0-dev gir1.2-glib-2.0 libgirepository1.0-dev li
 sudo apt-get -y install python-gi python3-gi gstreamer1.0-tools gir1.2-gstreamer-1.0 gir1.2-gst-plugins-base-1.0 gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad gstreamer1.0-libav
 sudo apt-get -y install python-scipy ffmpeg
 sudo apt-get -y build-dep gstreamer1.0
-sudo apt-get -y install autoconf automake autopoint libbz2-dev libdv4-dev libfaac-dev libfaad-dev libgtk-3-dev libmjpegtools-dev libtag1-dev libasound2-dev libtool libvpx-dev libxv-dev libx11-dev libogg-dev libvorbis-dev libopencv-dev libcv-dev libhighgui-dev libv4l-dev pkg-config zlib1g-dev gtk-doc-tools yasm bison flex
+sudo apt-get -y install autoconf automake autopoint libbz2-dev libdv4-dev libfaac-dev libfaad-dev libgtk-3-dev libmjpegtools-dev libtag1-dev libasound2-dev libtool libvpx-dev libxv-dev libx11-dev libogg-dev libvorbis-dev libopencv-dev libcv-dev libhighgui-dev libv4l-dev pkg-config zlib1g-dev gtk-doc-tools yasm bison flex libtheora-dev faac libfaac0
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/
 export LD_LIBRARY_PATH=/usr/local/lib/
 git clone http://git.chromium.org/webm/libvpx.git
