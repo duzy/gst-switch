bc7a84d75a0e411b8126ee925d85641e1962b867
hyades <aayushahuja@gmail.com>
2013-09-13 01:36:05 +0530

    Modified isntall.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,7 @@
 #! /bin/bash -ex
 
 sudo apt-get -y install python-software-properties python-pip
-sudo pip install mock
-sudo pip install pytest-cov
-sudo pip install pytest-pep8
-sudo pip install pylint
+sudo pip install -r requirements.txt --use-mirrors
 echo 'yes' | sudo add-apt-repository ppa:gstreamer-developers/ppa
 sudo apt-get update
 sudo apt-get -y install libglib2.0-dev gir1.2-glib-2.0 libgirepository1.0-dev libglib2.0-0 
@@ -18,10 +15,10 @@ export LD_LIBRARY_PATH=/usr/local/lib/
 git clone http://git.chromium.org/webm/libvpx.git
 cd libvpx
 git checkout v1.2.0
-./configure --enable-shared --enable-vp8 --prefix=/usr/local
+./configure --enable-shared --enable-vp8
 [[ -f Makefile ]] || {
-	printf "Configure libvpx failed, no Makefile generated!!!\n"
-	exit -1
+  printf "Configure libvpx failed, no Makefile generated!!!\n"
+  exit -1
     }
 make
 sudo make install || {
@@ -32,13 +29,13 @@ cd ..
 git clone git://anongit.freedesktop.org/gstreamer/gstreamer
 cd gstreamer
 git checkout 6c11da1
-./autogen.sh --prefix=/usr/local || {
+./autogen.sh || {
 printf "Failed to do autogen!!!\n"
 exit -1
 }
 [[ -f Makefile ]] || {
-	printf "Configure gstreamer failed, no Makefile generated!!!\n"
-	exit -1
+  printf "Configure gstreamer failed, no Makefile generated!!!\n"
+  exit -1
     }
 make clean
 make || {
@@ -53,13 +50,13 @@ cd ..
 git clone git://anongit.freedesktop.org/gstreamer/gst-plugins-base
 cd gst-plugins-base
 git checkout a8df760
-./autogen.sh --prefix=/usr/local || {
+./autogen.sh || {
 printf "Failed to do autogen!!!\n"
 exit -1
 }
 [[ -f Makefile ]] || {
-	printf "Configure gst-plugins-base failed, no Makefile generated!!!\n"
-	exit -1
+  printf "Configure gst-plugins-base failed, no Makefile generated!!!\n"
+  exit -1
     }
 make clean
 make || {
@@ -74,13 +71,13 @@ cd ..
 git clone git://anongit.freedesktop.org/gstreamer/gst-plugins-good
 cd gst-plugins-good
 git checkout d14d4c4
-./autogen.sh --enable-experimental --prefix=/usr/local || {
+./autogen.sh --enable-experimental || {
 printf "Failed to do autogen!!!\n"
 exit -1
 }
 [[ -f Makefile ]] || {
-	printf "Configure gst-plugins-good failed, no Makefile generated!!!\n"
-	exit -1
+  printf "Configure gst-plugins-good failed, no Makefile generated!!!\n"
+  exit -1
     }
 make clean
 make || {
@@ -95,13 +92,13 @@ cd ..
 git clone git://anongit.freedesktop.org/gstreamer/gst-plugins-ugly
 cd gst-plugins-ugly
 git checkout 68985ba
-./autogen.sh --prefix=/usr/local || {
+./autogen.sh || {
 printf "Failed to do autogen!!!\n"
 exit -1
 }
 [[ -f Makefile ]] || {
-	printf "Configure gst-plugins-ugly failed, no Makefile generated!!!\n"
-	exit -1
+  printf "Configure gst-plugins-ugly failed, no Makefile generated!!!\n"
+  exit -1
     }
 make clean
 make || {
@@ -117,13 +114,13 @@ git clone https://github.com/duzy/gst-plugins-bad.git
 cd gst-plugins-bad
 git checkout speakertrack_new
 git checkout 
-./autogen.sh --prefix=/usr/local || {
+./autogen.sh || {
 printf "Failed to do autogen!!!\n"
 exit -1
 }
 [[ -f Makefile ]] || {
-	printf "Configure gst-plugins-bad failed, no Makefile generated!!!\n"
-	exit -1
+  printf "Configure gst-plugins-bad failed, no Makefile generated!!!\n"
+  exit -1
     }
 make clean
 make || {
@@ -137,7 +134,7 @@ sudo make install || {
 cd ..
 git clone https://github.com/hyades/gst-switch.git
 cd gst-switch
-./autogen.sh --prefix=/usr/local || {
+./autogen.sh || {
 printf "Failed to do autogen!!!\n"
 exit -1
 }
@@ -150,3 +147,5 @@ sudo make install || {
        printf "make install of gstswitch failed!!!\n"
        exit -1
     }
+# sudo cp /usr/local/lib/girepository-1.0/*.* /usr/lib/girepository-1.0/
+
