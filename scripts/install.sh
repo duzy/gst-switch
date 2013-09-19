f93f7a774c997cb93f56e7982145d4e2700b7314
Aayush Ahuja <aayushahuja@gmail.com>
2013-09-19 22:02:55 +0530

    Update install.sh [ci skip]

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,8 +5,7 @@ sudo pip install mock
 sudo pip install pytest-cov
 sudo pip install pytest-pep8
 sudo pip install pylint
-sudo apt-get -y install libglib2.0-dev gir1.2-glib-2.0 libgirepository1.0-dev libglib2.0-0
-sudo apt-get -y install python-gi
+sudo apt-get -y install libglib2.0-dev gir1.2-glib-2.0 libgirepository1.0-dev libglib2.0-0 python-gi
 sudo apt-get -y install python-scipy ffmpeg
 sudo apt-get -y install autoconf automake autopoint libbz2-dev libdv4-dev libfaac-dev libfaad-dev libgtk-3-dev libmjpegtools-dev libtag1-dev libasound2-dev libtool libvpx-dev libxv-dev libx11-dev libogg-dev libvorbis-dev libopencv-dev libcv-dev libhighgui-dev libv4l-dev pkg-config zlib1g-dev gtk-doc-tools yasm bison flex
 export PKG_CONFIG_PATH=/usr/lib/pkgconfig/
@@ -111,8 +110,6 @@ sudo make install || {
 cd ..
 git clone https://github.com/hyades/gst-plugins-bad.git
 cd gst-plugins-bad
-git checkout speakertrack_new
-git checkout
 ./autogen.sh --prefix=/usr || {
 printf "Failed to do autogen!!!\n"
 exit -1
