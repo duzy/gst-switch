a918da0bde36a219f7df4a0db43c1f920fb075cf
Aayush Ahuja <aahuja@groupon.com>
2014-11-02 20:03:19 +0530

    running hyades/ instead of timvideos/

--- scripts/install2.sh
+++ scripts/install2.sh
@@ -2,7 +2,7 @@
 mkdir build
 cd build
 sudo apt-get install git python-pip
-git clone https://github.com/timvideos/gst-switch.git
+git clone https://github.com/hyades/gst-switch.git
 cd gst-switch
 chmod +x .travis-setup.sh
 ./.travis-setup.sh
