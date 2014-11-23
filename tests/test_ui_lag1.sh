fda7e1a4d9b91d53a89724792229140e4c35cade
Carl Karsten <carl@personnelware.com>
2014-11-23 13:32:09 -0600

    test_ui_lag1: Add more sources, add source # to each.

--- tests/test_ui_lag1.sh
+++ tests/test_ui_lag1.sh
@@ -1,36 +1,31 @@
 #!/bin/bash -ex
 
-# demos issue:  lag between thumb and canvas #30 
+# test_ui_lag1.sh
+# demos issue #30: lag between thumb and canvas 
 
 cd ../tools
 
 ./gst-switch-srv & srvpid=$! 
 sleep 5
 ./gst-switch-ui & uipid=$!
-sleep 5
-gst-launch-1.0 videotestsrc pattern=1 is-live=1 \
-        ! timeoverlay \
-        ! clockoverlay time-format="%S" font-desc="Sans 240" \
-        ! video/x-raw, width=300, height=200 \
-        ! gdppay \
-        ! tcpclientsink port=3000 \
-& src1pid=$!
 
-sleep 20
-
-gst-launch-1.0 videotestsrc pattern=18 is-live=1 \
-        ! timeoverlay \
-        ! clockoverlay time-format="%S" font-desc="Sans 240" \
-        ! video/x-raw, width=300, height=200 \
-        ! gdppay \
-        ! tcpclientsink port=3000 \
-& src2pid=$!
-
-pgrep gst
-echo $srvpid $uipid $src1pid $src2pid
+# srcpid=()
+for i in 1 2 3 4 5 6; do
+  sleep 2
+  gst-launch-1.0 videotestsrc pattern=18 is-live=1 \
+    ! textoverlay text="s ${i}" \
+        halignment=2 valignment=2 font-desc="Sans 40" \
+    ! timeoverlay font-desc="Sans 40" \
+    ! clockoverlay time-format="%S" font-desc="Sans 240" \
+    ! video/x-raw, width=300, height=200 \
+    ! gdppay \
+    ! tcpclientsink port=3000 \
+  & srcpid[i]=$!
+done
 
 read -p "press enter to kill all the processes:"
 
-kill $srvpid $uipid $src1pid $src2pid
-pgrep gst
+kill $srvpid $uipid 
+for i in 1 2 3 4 5 6; do kill ${srcpid[i]}; done
+
 
