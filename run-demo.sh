6513df42c8ba418c52e6af5f4406f3c9d99a30ef
Tim 'mithro' Ansell <mithro@mithis.com>
2015-01-10 08:23:19 +1300

    Making run-demo very slightly nicer.

--- run-demo.sh
+++ run-demo.sh
@@ -2,20 +2,26 @@
 
 # FIXME: Rewrite using the Python API so this is reliable.
 
-cd tools
+killall gst-switch-srv || true
+sleep 2
+./tools/gst-switch-srv -r &
+sleep 2
+./tools/gst-switch-ui &
+sleep 2
 
-./gst-switch-srv &
-sleep 5
-./gst-switch-ui &
-sleep 5
+gst-launch-1.0 audiotestsrc is-live=true \
+        ! audioconvert \
+        ! audio/x-raw,rate=48000,channels=2,layout=interleaved \
+        ! gdppay \
+        ! tcpclientsink port=4000 \
+&
+
 gst-launch-1.0 videotestsrc pattern=1 is-live=1 \
         ! timeoverlay \
         ! video/x-raw, width=300, height=200 \
         ! gdppay \
         ! tcpclientsink port=3000 \
 &
-
-sleep 20
 
 gst-launch-1.0 videotestsrc pattern=18 is-live=1 \
         ! timeoverlay \
@@ -24,4 +30,5 @@ gst-launch-1.0 videotestsrc pattern=18 is-live=1 \
         ! tcpclientsink port=3000 \
 &
 
-
+wait
+killall gst-switch-srv || true
